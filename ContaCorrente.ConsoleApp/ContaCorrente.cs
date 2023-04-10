9a99dd93e9f7d356a7e164c3e11ed7541ceba6e5
Anderson Medeiros <andersonviniciusmedeiros97@gmail.com>
2023-04-10 15:10:32 -0300

    Add project files.

---
+++ ContaCorrente.ConsoleApp/ContaCorrente.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContaCorrente.ConsoleApp
+{
+    internal class ContaCorrente
+    {
+        private double saldo;
+        private int numero;
+        private int limite;
+        private bool EhEspecial;
+        private Movimentacao[] movimentacoes;
+        private int numMovimentacoes;
+        public ContaCorrente(double sld, int num, int lmt, bool especial)
+        {
+            saldo = sld;
+            numero = num;
+            limite = lmt;
+            EhEspecial = especial;
+            movimentacoes = new Movimentacao[10];
+            numMovimentacoes = 0;
+        }
+
+        public void Depositar(double valor)
+        {
+            saldo += valor;
+            movimentacoes[numMovimentacoes] = new Movimentacao(valor, false);
+            numMovimentacoes++;
+        }
+
+        public bool Sacar(double valor)
+        {
+            if (saldo - valor >= limite)
+            {
+                saldo -= valor;
+                movimentacoes[numMovimentacoes] = new Movimentacao(valor, true);
+                numMovimentacoes++;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool Transferir(double valor, ContaCorrente contaDestino)
+        {
+            if (saldo - valor >= limite)
+            {
+                saldo -= valor;
+                movimentacoes[numMovimentacoes] = new Movimentacao(valor, true);
+                numMovimentacoes++;
+                contaDestino.Depositar(valor);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public double GetSaldo()
+        {
+            return saldo;
+        }
+
+        public Movimentacao[] GetMovimentacoes() { 
+            return movimentacoes.Take(numMovimentacoes).ToArray();
+        }
+
+        internal class Movimentacao
+        {
+            public double Valor;
+            public bool EhDebito;
+
+            public  Movimentacao(double valor, bool ehDebito)
+            {
+                Valor = valor;
+                EhDebito = ehDebito;
+            }
+        }
+    }
+}
---
+++ ContaCorrente.ConsoleApp/Program.cs
@@ -0,0 +1,58 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace ContaCorrente.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente conta1 = new ContaCorrente(1000, 12, 0, true);
+            ContaCorrente conta2 = new ContaCorrente(500, 15, 0, false);
+            conta1.Depositar(200);
+
+            bool saqueRealizado = conta1.Sacar(100);
+
+            if(saqueRealizado)
+            {
+                Console.WriteLine("Saque realizado com sucesso!!");
+            }
+            else
+            {
+                Console.WriteLine("Você não tem saldo suficiente!!");
+            }
+
+            bool transferenciaRealizada = conta1.Transferir(230, conta2);
+
+            if(transferenciaRealizada)
+            {
+                Console.WriteLine("Transferência realizada com sucesso!!");
+            }
+            else
+            {
+                Console.WriteLine("Você não tem saldo suficiente!!");
+            }
+
+            Console.WriteLine($"Saldo Conta 1: {conta1.GetSaldo():C}");
+            Console.WriteLine($"Saldo Conta 2: {conta2.GetSaldo():C}");
+
+
+            ContaCorrente.Movimentacao[] movimentacoes1 = conta1.GetMovimentacoes();
+            ContaCorrente.Movimentacao[] movimentacoes2 = conta2.GetMovimentacoes();
+
+            Console.WriteLine("Movimentações Conta 1:");
+            foreach (var movimentacao in movimentacoes1)
+            {
+                string tipo = movimentacao.EhDebito ? "Débito" : "Crédito";
+                Console.WriteLine($"{tipo}: {movimentacao.Valor:C}");
+            }
+
+            Console.WriteLine("Movimentações Conta 2:");
+            foreach (var movimentacao in movimentacoes2)
+            {
+                string tipo = movimentacao.EhDebito ? "Débito" : "Crédito";
+                Console.WriteLine($"{tipo}: {movimentacao.Valor:C}");
+            }
+            Console.ReadLine();
+        }
+    }
+}
